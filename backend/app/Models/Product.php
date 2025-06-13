9a6b0e7629308bc63901e372f5eb29141f445a4f
MSalah <msalah0022@gmail.com>
2025-06-14 02:34:07 +0300

    feat: create Product model with migrations, factory

---
+++ backend/app/Models/Product.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'price',
+        'image_url',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'price' => 'float',
+            'image_url' => 'string',
+            'title' => 'string',
+        ];
+    }
+}
